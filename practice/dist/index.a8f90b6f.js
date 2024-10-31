500383a088e1317f463b397b310ae10ab243a244
diepphan <ngocdiepphan180402@gmail.com>
2024-10-31 21:48:00 +0700

    Update package json

---
+++ practice/dist/index.a8f90b6f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".icon-menu"),c=document.querySelector(".nav-links-mb");e.addEventListener("click",()=>{c.classList.toggle("active")}),c.addEventListener("click",()=>{c.classList.remove("active")});
+//# sourceMappingURL=index.a8f90b6f.js.map
